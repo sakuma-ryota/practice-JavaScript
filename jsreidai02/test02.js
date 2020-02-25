efc70fc4bc623ba80fe1d61bbf22298ff213d6d6
”sakuma” <srff7cloud@yahoo.co.jp>
2020-02-25 11:10:22 +0900

    フロントエンド02例題#2

--- jsreidai02/test02.js
+++ jsreidai02/test02.js
@@ -1,7 +1,9 @@
 (function($, window) {
     $(function() {
         //処理
-        $('h2').show();
-        $('#index').find('li').eq(0).hide()
+       $('#index').find('li').each(function() {
+           //ループ内の処理
+           alert($(this).text());
+       });
     });
 })(jQuery, window);
