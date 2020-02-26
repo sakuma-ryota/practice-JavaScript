41766ba7a1d905293dc99b65191c8d2d721cb686
”sakuma” <srff7cloud@yahoo.co.jp>
2020-02-26 20:21:12 +0900

    2/26終了分

--- js_kadai03/jskadai5・6.js
+++ js_kadai03/jskadai5・6.js
@@ -1,16 +1,18 @@
 (function($, window) {
     $(function() {
-      // ここに処理を書きます
-      $('#zip1').jpostal({
-          postcode : [
-              '#zip1',
-              '#zip2'
-          ],
-          address : {
-              '#pref'  : '%3',
-              '#city'  : '%4',
-              '#street'  : '%5'
-          }
-      });
+        // ここに処理を書きます
+        $('#btn').on('click', function() {
+            $('#zip1').jpostal({
+                postcode : [
+                    '#zip1',
+                    '#zip2'
+                ],
+                address : {
+                    '#pref'  : '%3',
+                    '#city'  : '%4',
+                    '#street'  : '%5'
+                }
+            });
+        });
     });
   })(jQuery, window);
